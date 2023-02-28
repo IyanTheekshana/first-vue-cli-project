f4230c71f7829b77483fd815a92b5c33569dad2b
Iyan Theekshana <iyan.dev123@gmail.com>
2023-02-28 17:03:04 +0100

    added App.vue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>My Friends</h2>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenzo",
+          phone: "01234567891",
+          email: "manuel@localhost.it",
+        },
+        {
+          id: "john",
+          name: "John Cena",
+          phone: "01234567891",
+          email: "john@localhost.it",
+        },
+      ],
+    };
+  },
+};
+</script>
