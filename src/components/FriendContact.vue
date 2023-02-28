284376d88128a24282d4aabe9fb52e2d6ddb44a3
Iyan Theekshana <iyan.dev123@gmail.com>
2023-02-28 18:37:33 +0100

    passing data though components parent -> child

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails()">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>Phone: {{ friend.phone }}</li>
+      <li>Email: {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["name", "phoneNumber", "emailAddress"],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: this.name,
+        name: this.name,
+        phone: this.phoneNumber,
+        email: this.emailAddress,
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
